c391c8b6cc094786ecf25f467cb1dc9e1c707ddd
ae0118 <abrahamescamilla@my.unt.edu>
2018-09-10 13:36:04 -0500

    Exclude bad emails
    Edit script to exclude emails that are empty or NULL

--- STAGE_EMAIL.sql
+++ STAGE_EMAIL.sql
@@ -3,16 +3,12 @@
 filepath:
 \\departments\32\UB Software Replacement\Conversion\sql
 ------------------------------------------------------
-
-version="1.0.0"
-Last Update: August 15, 2018               
-
-
+version="1.0.1"
+Last Update: September 6, 2018               
 ------------------------------------------------------
 Purpose:
 	Extract email addresses from Inhance to place in
-	a staging table for Systems & Software enQuesta v6   
-	                         
+	a staging table for Systems & Software enQuesta v6 
 ------------------------------------------------------
 Instructions:
 	Run against OASIS (Principal, Synchronized)
@@ -29,6 +25,7 @@ Assumptions:
 ------------------------------------------------------
 HISTORY:
 	August 15, 2018 - v1.0.0  - First Creation
+	September 6,2018 - v1.0.1 - add WHERE clause, comments
 ------------------------------------------------------
 TODO:
 	
@@ -38,12 +35,12 @@ SELECT
 	'~' + LEFT(primaryemail.p_email_address, 254) + '~' AS '~EMAILADDRESS~',
 	1 AS '~EMAILCODE~',
 	1 AS '~PRIORITY~',
-	CONVERT(char(10), GetDate(),126) AS '~UPDATEDATE~'
+	CONVERT(char(10), GetDate(),126) AS '~UPDATEDATE~' -- YYYY-MM-DD
 FROM
 	vw_customer primaryemail
 WHERE 
-	(primaryemail.p_email_address LIKE '%@%' AND primaryemail.p_email_address LIKE '%.%')
-	AND LEN(primaryemail.p_email_address) > 0
+	(primaryemail.p_email_address LIKE '%@%' AND primaryemail.p_email_address LIKE '%.%') -- email must have an '@' and '.' to be valid
+	AND LEN(primaryemail.p_email_address) > 0 -- avoid empty emails
 
 UNION ALL
 SELECT
@@ -51,9 +48,9 @@ SELECT
 	'~' + LEFT(secondaryemail.s_email_address, 254) + '~' AS '~EMAILADDRESS~',
 	1 AS '~EMAILCODE~',
 	2 AS '~PRIORITY~',
-	CONVERT(char(10), GetDate(),126) AS '~UPDATEDATE~'
+	CONVERT(char(10), GetDate(),126) AS '~UPDATEDATE~' -- YYYY-MM-DD
 FROM
 	vw_customer secondaryemail
 WHERE
-	(secondaryemail.s_email_address LIKE '%%'AND secondaryemail.s_email_address LIKE '%.%')
-	AND LEN(secondaryemail.s_email_address) > 0
+	(secondaryemail.s_email_address LIKE '%@%'AND secondaryemail.s_email_address LIKE '%.%') -- email must have an '@' and '.' to be valid
+	AND LEN(secondaryemail.s_email_address) > 0 -- avoid empty emails
